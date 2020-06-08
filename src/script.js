8ee67bab6bff8e338e07d366bb99b2f32e91a346
Lucas Paulo <lucaspaulodev@gmail.com>
2020-06-08 06:09:00 -0300

    Recent updates
    Updated interface and logic

--- src/script.js
+++ src/script.js
@@ -1,40 +1,41 @@
-var btn = document.querySelector('button')
-var topic = document.querySelector('input')
+const btn = document.querySelector('button')
+const topic = document.querySelector('input')
 
-async function createCard(news){
-    var card = document.createElement('div')
+function createCard(news){
+    const card = document.createElement('div')
     card.classList.add('new')
-    var imagebox = document.createElement('div')
-    imagebox.classList.add('imagebox')
-    var img = document.createElement('img')
+    const img = document.createElement('img')
     img.src = `${news.urlToImage}`
-    imagebox.appendChild(img)
-    card.appendChild(imagebox)
-    var title = document.createElement('a')
+    card.appendChild(img)
+    const title = document.createElement('a')
     title.href = `${news.url}`
     title.innerHTML = `${news.title}`
     card.appendChild(title)
+    const source = document.createElement('div')
+    source.classList.add('source')
+    source.innerHTML = `${news.source.name}`
+    card.appendChild(source)
     document.querySelector('.news').appendChild(card)
 }
 
-async function getNews(url){
-    await fetch(url,{mode: 'cors'}).then((res)=>{
+function getNews(url){
+    fetch(url,{mode: 'cors'}).then((res)=>{
         if(res.ok){
             return res.json()
         }
         else{'Network response was not ok.'}
-    }).then(async (data)=>{
-        var news = data.articles.slice(0, 3)
+    }).then((data)=>{
+        const news = data.articles.slice(0, 10)
         news.map(nw => createCard(nw))
     }).catch(err => {console.log(err)})
 }
 
-topic.addEventListener('click', ()=>{
-    btn.disabled = false;
-})
-
 btn.addEventListener('click', ()=>{
     const apiKey = 'f127fddc29c24f2b8618a28cc8718d93'
-    let url = `https://newsapi.org/v2/everything?q=${topic.value}&apiKey=${apiKey}`
-    if(topic.value !== ''){getNews(url)}    
+    let url = `https://newsapi.org/v2/top-headlines?q=${topic.value}&apiKey=${apiKey}`
+    if(topic.value !== ''){
+        document.querySelector('.search').classList.add('up')
+        document.querySelector('.news').innerText = ''
+        getNews(url)
+    }    
 })
